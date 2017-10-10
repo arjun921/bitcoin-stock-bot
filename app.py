1727d9030a81f810d06aac31fc68864abcc373b8
Arjun S <arjun921@gmail.com>
2017-10-10 13:49:05 +0530

    Update webservice

--- app.py
+++ app.py
@@ -28,15 +28,15 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "oil.price":
+    if req.get("result").get("action") != "shipping.cost":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
 
-    cost = {'Current Price':100, 'Closing Price':200, 'Opening Price':300, 'Profit Percentage':400, 'Loss Percentage':500}
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The" + zone +"  oil price " + " is " + str(cost[zone]) + " euros."
+    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
     print("Response:")
     print(speech)
