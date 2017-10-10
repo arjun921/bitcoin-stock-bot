79e823b44a3724af2c2c9815fe91c5f5d5f2ba30
Arjun S <arjun921@gmail.com>
2017-10-10 13:51:03 +0530

    Update webservice

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+    cost = {'Current Price':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
     speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
