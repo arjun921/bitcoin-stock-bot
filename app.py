3a6e662f0c129d3a7938a519451d3a1aa2d46117
Rupam Rajshree <rupamrules73@gmail.com>
2017-11-01 14:28:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def load_page():
 def fetch_current():
 	webpage = ""
 	soup = BeautifulSoup(load_page(), 'html.parser')
-	x = soup.find_all('span',class_="arial_26 inlineblock pid-945629-last")
+	x = soup.find_all('span',class_="arial_26 inlineblock pid-8849-last")
 	return x[0].contents[0]
 
 
